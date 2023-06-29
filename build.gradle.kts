5d2b322c9b9db32f8dd265123a63e6e08716bc63
ovnny2 <ovnnychannel@gmail.com>
2023-06-28 22:46:36 -0300

    refatorando o arquivo build gradle com erros na declaração de versões de dependência

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,46 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.5"
+	id("org.springframework.boot") version "2.7.13"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	id("com.starter.config") version "0.58.0"
-	kotlin("jvm") version "1.8.10"
 	kotlin("plugin.spring") version "1.8.10"
+	kotlin("jvm") version "1.8.10"
 }
 
 group = "com.ovnny"
 version = "0.0.1"
-java.sourceCompatibility = JavaVersion.VERSION_17
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
 
 repositories {
 	mavenCentral()
 }
 
+extra["springCloudVersion"] = "2021.0.7"
+
 dependencies {
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.cloud:spring-cloud-starter-config:3.1.7")
-
-
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
 	}
 }
 
