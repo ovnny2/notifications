e6207354bec2141d098ee85ff76a59785849639b
ovnny2 <ovnnychannel@gmail.com>
2023-07-06 15:01:46 -0300

    refatoramento de error handling - retirando parâmetros hard coded

--- src/main/kotlin/com/ovnny/notifications/exception/NotificationConflitOnDeletionException.kt
+++
@@ -1,5 +0,0 @@
-package com.ovnny.notifications.exception
-
-import org.springframework.http.HttpStatus
-
-class NotificationConflitOnDeletionException(override val message: String?, val status: HttpStatus) : RuntimeException(message)
