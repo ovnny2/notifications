33e8766de6802fc521c622edc18a9361a66cb1b0
ovnny2 <ovnnychannel@gmail.com>
2023-07-06 15:07:23 -0300

    criação de função auxiliar para resolver mensagens de erro - MessageSource

--- src/main/kotlin/com/ovnny/notifications/advice/NotificationControllerAdviser.kt
+++ src/main/kotlin/com/ovnny/notifications/advice/NotificationControllerAdviser.kt
@@ -1,36 +1,40 @@
 package com.ovnny.notifications.advice
 
 import com.ovnny.notifications.exception.MessageError
-import com.ovnny.notifications.exception.NotificationConflitOnDeletionException
 import com.ovnny.notifications.exception.NotificationNotFoundException
-import com.ovnny.notifications.advice.msg.NotificationMessages
+import org.springframework.context.MessageSource
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import java.util.*
 
 
 @Component
 @ControllerAdvice
-class NotificationControllerAdviser {
+class NotificationControllerAdviser(
+    private val messageSource: MessageSource
+) {
 
     @ExceptionHandler(RuntimeException::class)
     fun handle(ex: Exception): ResponseEntity<MessageError> {
-        return ResponseEntity.internalServerError().body(
-            MessageError(NotificationMessages.GENERIC_MESSAGE.msg, HttpStatus.INTERNAL_SERVER_ERROR)
-        )
+        val messageError = MessageError(resolveMessage(ex), HttpStatus.INTERNAL_SERVER_ERROR)
+
+        return ResponseEntity.status(messageError.status).body(messageError)
     }
 
     @ExceptionHandler(NotificationNotFoundException::class)
     fun handle(ex: NotificationNotFoundException): ResponseEntity<MessageError> {
-        val response = MessageError(NotificationMessages.NOT_FOUND_MESSAGE.msg, ex.status)
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response)
+        val messageError = MessageError(resolveMessage(ex), ex.httpStatus)
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(messageError)
     }
 
-    @ExceptionHandler(IllegalStateException::class)
-    fun handle(ex: NotificationConflitOnDeletionException): ResponseEntity<MessageError> {
-        return ResponseEntity.unprocessableEntity()
-            .body(MessageError(NotificationMessages.RULE_BUSINESS_BROKEN_MESSAGE.msg, ex.status))
+    fun resolveMessage(ex: Exception): String {
+
+        val locale = Locale("pt")
+
+        return messageSource.getMessage("${ex::class.simpleName}", null, locale)
     }
 }
